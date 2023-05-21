c884182da594fb727411ce1b70ca92dfc490ee9a
Sajal Kumar Bajaj <bajajsajal3369@gmail.com>
2023-05-21 11:19:55 +0530

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using inventorySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inventorySystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        // create database object db 
+        inv_systemEntities db = new inv_systemEntities();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        // Display Product List Method
+        [HttpGet]
+        public ActionResult DisplayProduct()
+        {
+            List<tbl_product> list = db.tbl_product.OrderByDescending(x => x.proID).ToList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+
+            return View();
+        }
+
+        // Create Product Method
+        [HttpPost]
+        public ActionResult CreateProduct(tbl_product pro)
+        {
+            db.tbl_product.Add(pro);
+            db.SaveChanges();
+
+            return RedirectToAction("DisplayProduct");
+        }
+        [HttpGet]
+        // Update Product Method
+        public ActionResult UpdateProduct(int id)
+        {
+            tbl_product pr = db.tbl_product.Where(x => x.proID == id).SingleOrDefault();
+
+            return View(pr);
+        }
+
+        [HttpPost]
+        // Update Product Method
+        public ActionResult UpdateProduct(int id,tbl_product pro)
+        {
+            tbl_product pr = db.tbl_product.Where(x => x.proID == id).SingleOrDefault();
+            pr.proName = pro.proName;
+            pr.proQty = pro.proQty;
+            db.SaveChanges();
+            //return View(pr);
+            return RedirectToAction("DisplayProduct");
+        }
+
+        // Detail or view  Method 
+        [HttpGet]
+        public ActionResult ViewProduct(int id)
+        {
+            tbl_product pr = db.tbl_product.Where(x => x.proID == id).SingleOrDefault();        
+            return View(pr);
+         
+        }
+        //Delete products 
+
+
+        [HttpGet]
+        // Update Product Method
+        public ActionResult DeleteProduct(int id)
+        {
+            tbl_product pr = db.tbl_product.Where(x => x.proID == id).SingleOrDefault();
+
+            return View(pr);
+        }
+
+        [HttpPost]
+        // Update Product Method
+        public ActionResult DeleteProduct(int id,tbl_product pro)
+        {
+            
+            db.tbl_product.Remove(db.tbl_product.Where(x=>x.proID == id).SingleOrDefault());
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
+
+    }
+} 
---
+++ Controllers/PurchaseController.cs
@@ -0,0 +1,96 @@
+﻿using inventorySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inventorySystem.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        // create database object db 
+        inv_systemEntities db = new inv_systemEntities();
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Display Product List Method
+        [HttpGet]
+        public ActionResult DisplayPurchase()
+        {
+            List<tbl_purchase> list = db.tbl_purchase.OrderByDescending(x => x.poID).ToList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult CreatePurchase()
+        {
+
+            return View();
+        }
+
+        // Create Product Method
+        [HttpPost]
+        public ActionResult CreatePurchase(tbl_purchase po)
+        {
+            db.tbl_purchase.Add(po);
+            db.SaveChanges();
+
+            return RedirectToAction("DisplayPurchase");
+        }
+        [HttpGet]
+        // Update Product Method
+        public ActionResult UpdatePurchase(int id)
+        {
+            tbl_purchase po = db.tbl_purchase.Where(x => x.poID == id).SingleOrDefault();
+
+            return View(po);
+        }
+
+        [HttpPost]
+        // Update Product Method
+        public ActionResult UpdatePurchase(int id, tbl_purchase pur)
+        {
+            tbl_purchase pr = db.tbl_purchase.Where(x=>x.poID == id).SingleOrDefault();
+            pr.poName = pur.poName;
+            pr.poDate = pur.poDate;
+            pr.proQty = pur.proQty;
+            db.SaveChanges();
+            //return View(pr);
+            return RedirectToAction("DisplayPurchase");
+        }
+
+        // Detail or view  Method 
+        [HttpGet]
+        public ActionResult ViewPurchase(int id)
+        {
+            tbl_purchase pr = db.tbl_purchase.Where(x => x.poID == id).SingleOrDefault();
+            return View(pr);
+
+        }
+        //Delete products 
+
+
+        [HttpGet]
+        // Update Product Method
+        public ActionResult DeletePurchase(int id)
+        {
+            tbl_purchase pr = db.tbl_purchase.Where(x => x.poID == id).SingleOrDefault();
+
+            return View(pr);
+        }
+
+        [HttpPost]
+        // Update Product Method
+        public ActionResult DeletePurchase(int id, tbl_product pro)
+        {
+      
+            db.tbl_purchase.Remove(db.tbl_purchase.Where(x => x.poID == id).SingleOrDefault());
+            db.SaveChanges();
+            return RedirectToAction("DisplayPurchase");
+        }
+    }
+}
